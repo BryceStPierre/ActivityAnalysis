5a115e96ca6d90689c3a46b69c6e817b837bdc78
Bryce St. Pierre <brycelstpierre@gmail.com>
2019-03-03 09:44:36 -0500

    Added initial solution and projects.

---
+++ EnvironmentManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnvironmentManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
