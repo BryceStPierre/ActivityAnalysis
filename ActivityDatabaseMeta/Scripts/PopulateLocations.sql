077244fe18a60c1c1cb5c91ec309243689d70338
Bryce St. Pierre <brycelstpierre@gmail.com>
2019-03-09 10:35:10 -0500

    Updated data source infrastructure.

---
+++ ActivityDatabaseMeta/Scripts/PopulateLocations.sql
@@ -0,0 +1,9 @@
+﻿TRUNCATE TABLE [Environment].[Locations]
+GO
+
+SET IDENTITY_INSERT [Environment].[Locations] ON 
+GO
+INSERT [Environment].[Locations] ([Id], [Name], [Path], [Updated], [Created]) VALUES (1, N'Root', N'C:/Users/bryce/Data', GETDATE(), GETDATE())
+GO
+SET IDENTITY_INSERT [Environment].[Locations] OFF
+GO
