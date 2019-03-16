206633d72f2cdbdea180c6e9138c732fe655c9e4
Bryce St. Pierre <brycelstpierre@gmail.com>
2019-03-16 09:26:02 -0400

    Reconfigured ETL project and added inital Source tables.

---
+++ ActivityDatabaseStage/Source/ApplePodcastHistory.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Source].[ApplePodcastHistory] (
+	[Feed URL] [VARCHAR](200) NULL,
+	[Episode ID] [VARCHAR](200) NULL,
+	[Visible?] [VARCHAR](3) NULL,
+	[Marked as played on] [DATETIME] NULL,
+	[Manually set] [VARCHAR](3) NULL,
+	[Is New?] [VARCHAR](3) NULL,
+	[Last played on] [DATETIME] NULL,
+	[Playback position] [FLOAT] NULL,
+	[Play count] [INT] NULL,
+	[Has been played?] [VARCHAR](3) NULL
+)
---
+++ ActivityDatabaseStage/Source/ApplePodcasts.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [Source].[ApplePodcasts] (
+	[Last updated] [DATETIME] NULL,
+	[Added on] [DATETIME] NULL,
+	[Feed URL] [VARCHAR](MAX) NULL,
+	[Title] [VARCHAR](75) NULL,
+	[Subscribed?] [VARCHAR](3) NULL,
+	[Last touched on] [DATETIME] NULL,
+	[Show type] [VARCHAR](50) NULL,
+	[Sort] [VARCHAR](15) NULL,
+	[Playback newest to oldest] [VARCHAR](3) NULL
+)
