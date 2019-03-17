0b3d5be7c2df40c503deabec5016e16ebe60f754
Bryce St. Pierre <brycelstpierre@gmail.com>
2019-03-16 20:19:07 -0400

    Renamed projects.

---
+++ ActivityAnalysisMeta/Scripts/PopulateDataSets.sql
@@ -0,0 +1,8 @@
+﻿SET IDENTITY_INSERT [Integration].[DataSets] ON 
+GO
+INSERT [Integration].[DataSets] ([Id], [DataSource], [Name], [Label], [Description], [Path], [Updated], [Created]) VALUES (1, N'Apple', N'Podcasts', N'Podcasts', N'My podcast subscriptions.', N'C:\Users\bryce\Data\2019\03\ApplePodcasts\Your Podcasts.csv', CAST(N'2019-03-09T09:39:35.967' AS DateTime), CAST(N'2019-03-09T09:39:35.967' AS DateTime))
+GO
+INSERT [Integration].[DataSets] ([Id], [DataSource], [Name], [Label], [Description], [Path], [Updated], [Created]) VALUES (2, N'Apple', N'PodcastHistory', N'Podcast History', N'My podcast playstate history.', N'C:\Users\bryce\Data\2019\03\ApplePodcastHistory\Podcasts Playstate.csv', CAST(N'2019-03-09T09:40:47.623' AS DateTime), CAST(N'2019-03-09T09:40:47.623' AS DateTime))
+GO
+SET IDENTITY_INSERT [Integration].[DataSets] OFF
+GO
---
+++ ActivityAnalysisMeta/Scripts/PopulateDataSources.sql
@@ -0,0 +1,26 @@
+﻿SET IDENTITY_INSERT [Integration].[DataSources] ON 
+GO
+INSERT [Integration].[DataSources] ([Id], [Name], [Description], [Updated], [Created]) VALUES (1, N'Apple', N'My smartphone''s manufacturer.', CAST(N'2019-03-09T09:23:06.460' AS DateTime), CAST(N'2019-03-09T09:23:06.460' AS DateTime))
+GO
+INSERT [Integration].[DataSources] ([Id], [Name], [Description], [Updated], [Created]) VALUES (2, N'Telus', N'My cellular service provider.', CAST(N'2019-03-09T09:23:57.900' AS DateTime), CAST(N'2019-03-09T09:23:57.900' AS DateTime))
+GO
+INSERT [Integration].[DataSources] ([Id], [Name], [Description], [Updated], [Created]) VALUES (3, N'TekSavvy', N'My Internet service provider.', CAST(N'2019-03-09T09:24:18.300' AS DateTime), CAST(N'2019-03-09T09:24:18.300' AS DateTime))
+GO
+INSERT [Integration].[DataSources] ([Id], [Name], [Description], [Updated], [Created]) VALUES (4, N'Twitter', N'One of my social media accounts.', CAST(N'2019-03-09T09:25:07.640' AS DateTime), CAST(N'2019-03-09T09:25:07.640' AS DateTime))
+GO
+INSERT [Integration].[DataSources] ([Id], [Name], [Description], [Updated], [Created]) VALUES (5, N'Instagram', N'One of my social media accounts.', CAST(N'2019-03-09T09:25:11.063' AS DateTime), CAST(N'2019-03-09T09:25:11.063' AS DateTime))
+GO
+INSERT [Integration].[DataSources] ([Id], [Name], [Description], [Updated], [Created]) VALUES (6, N'Snapchat', N'One of my social media accounts.', CAST(N'2019-03-09T09:25:17.027' AS DateTime), CAST(N'2019-03-09T09:25:17.027' AS DateTime))
+GO
+INSERT [Integration].[DataSources] ([Id], [Name], [Description], [Updated], [Created]) VALUES (7, N'Facebook', N'One of my social media accounts.', CAST(N'2019-03-09T09:25:31.583' AS DateTime), CAST(N'2019-03-09T09:25:31.583' AS DateTime))
+GO
+INSERT [Integration].[DataSources] ([Id], [Name], [Description], [Updated], [Created]) VALUES (8, N'Netflix', N'My television streaming service.', CAST(N'2019-03-09T09:26:35.587' AS DateTime), CAST(N'2019-03-09T09:26:35.587' AS DateTime))
+GO
+INSERT [Integration].[DataSources] ([Id], [Name], [Description], [Updated], [Created]) VALUES (9, N'RBC', N'My banking institution.', CAST(N'2019-03-09T09:28:04.013' AS DateTime), CAST(N'2019-03-09T09:28:04.013' AS DateTime))
+GO
+INSERT [Integration].[DataSources] ([Id], [Name], [Description], [Updated], [Created]) VALUES (10, N'Google', N'My browser service.', CAST(N'2019-03-09T09:29:00.403' AS DateTime), CAST(N'2019-03-09T09:29:00.403' AS DateTime))
+GO
+INSERT [Integration].[DataSources] ([Id], [Name], [Description], [Updated], [Created]) VALUES (11, N'YouTube', N'My video service.', CAST(N'2019-03-09T09:32:55.723' AS DateTime), CAST(N'2019-03-09T09:32:55.723' AS DateTime))
+GO
+SET IDENTITY_INSERT [Integration].[DataSources] OFF
+GO
